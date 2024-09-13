452271482abfe66a6233658aac2c868296de98fa
bilikhasan <fatherlyy@hotmail.com.tr>
2024-09-13 23:24:25 +0300

    Add project files.

---
+++ MvcCv/Controllers/EgitimController.cs
@@ -0,0 +1,71 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    [Authorize]
+
+    public class EgitimController : Controller
+    {
+        // GET: Egitim
+        GenericRepository<TblEgitimlerim> repo = new GenericRepository<TblEgitimlerim>();
+        public ActionResult Index()  //Burası lısteleme sayfası
+        {
+            var egitim = repo.List();
+            return View(egitim);
+        }
+
+        [HttpGet]
+        public ActionResult EgitimEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult EgitimEkle(TblEgitimlerim p)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("EgitimEkle");
+            }
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+        public ActionResult EgitimSil(int id)
+        {
+            var egitim = repo.Find(x => x.ID == id);
+            repo.TDelete(egitim);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult EgitimDuzenle(int id)
+        {
+            var egitim = repo.Find(x => x.ID == id);
+            return View(egitim);
+        }
+
+        [HttpPost]
+        public ActionResult EgitimDuzenle(TblEgitimlerim p)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("EgitimDuzenle");
+            }
+
+            var egitim = repo.Find(x => x.ID == p.ID);
+            egitim.Baslik = p.Baslik;
+            egitim.Altbaslik1 = p.Altbaslik1;
+            egitim.Altbaslik2 = p.Altbaslik2;
+            egitim.Tarih = p.Tarih;
+            egitim.GNO = p.GNO;
+            repo.TUpdate(egitim);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcCv/Repositories/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using MvcCv.Models.Entity;
+
+namespace MvcCv.Repositories
+{
+    public class GenericRepository<T> where T:class,new()
+    {
+        DbCvEntities db =new DbCvEntities();
+
+        public List<T> List()             //Listeleme işlemi
+        {
+            return db.Set<T>().ToList();
+        }
+
+        public void TAdd (T p)          //Ekleme işlemi
+        {
+            db.Set<T>().Add(p);
+            db.SaveChanges();
+        }
+
+        public void TDelete (T p)      //Silme işlemi
+        {
+            db.Set<T>().Remove(p);
+            db.SaveChanges();
+        }
+
+        public T TGet (int id)          //id ye göre getirme işlemi
+        {
+            return db.Set<T>().Find(id);
+        }
+
+        public void TUpdate (T p)              //güncelleme işlemi
+        {
+            db.SaveChanges();
+        }
+
+        public T Find (Expression<Func<T,bool>> where)      //id ye göre silme
+        {
+            return db.Set<T>().FirstOrDefault(where);
+        }
+    }
+}
